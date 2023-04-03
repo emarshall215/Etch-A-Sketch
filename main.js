0aefe0016370c16345c417d670b948a697fff631
Ellis Marshall <ellis.marshall215@gmail.com>
2023-04-03 15:26:18 -0400

    reworked Javascript, button functionality

--- main.js
+++ main.js
@@ -1,19 +1,46 @@
-document.addEventListener('DOMContentLoaded',function(){
-    createBoard(32);
-    console.log('hi')
-})
- 
+let color = "black"
 
-    function createBoard(size){
-        let board = document.querySelector('.board');
+function createBoard(size){
+    let board = document.querySelector(".board")
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div)=> div.remove());
+board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+let amount = size * size
+for(let i = 0; i < amount; i++){
+    let square = document.createElement("div");
+    square.addEventListener("mouseover", colorSquare);
+    square.style.backgroundColor = "white";
+    board.insertAdjacentElement("beforeend", square);
+}
+}
 
+createBoard(16);
 
-        let numDivs = size * size;
-
-        for(let i = 0; i < numDivs; i++);
-            let div = document.createElement("div");
-            board.insertAdjacentElement("beforeend", div);
+function changeSize(input) {
+    if(input >= 2 || input <= 100){
+        createBoard(input);
+    } else{
+        console.log("invalid option");
     }
+}
+
+function colorSquare(){
+    if(color ==  "random"){
+        this.style.backgroundColor = `hsla(${Math.random() * 360}, 100%, 50%, 1)`;
+    } else {
+        this.style.backgroundColor = color;
+    }
+      
+}
+
+function changeColor(choice){
+    color = choice;
+}
+
+function resetBoard(){
+    let board = document.querySelector(".board")
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div)=> div.style.backgroundColor = "white");
+}
